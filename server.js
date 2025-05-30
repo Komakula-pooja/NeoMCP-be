c8b5c0c4d77068a294b7fe7a846476ea6f335139
Komakula-pooja <poojithakomakula21@gmail.com>
2025-05-30 15:33:00 +0530

    added deployed fe route

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173', 'https://neomcp.ai');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
